3ca5f2e716531c330016ca0dfa64dcd58b22fbe6
Horatio <hs@horatio.cn>
2024-06-10 19:19:41 +0800

    Update qq.php

--- avatar/qq.php
+++ avatar/qq.php
@@ -1,7 +1,15 @@
 <?php
 
 $qq = $_GET['qq'];
-
 $link = "https://q1.qlogo.cn/g?b=qq&nk={$qq}&s=640";
+
+if ($qq != null)
+{
+    header("Location:$link"); // 302 跳转
+}
+else
+{
+    echo "请在url后加入?=QQ号";
+}
 
 ?>
